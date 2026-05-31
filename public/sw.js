8e1f9b5cdb149cf4f9c3cd495bd1ce5bfc145ef6
Daniel Logan <dlogan@DanielsMBP.local>
2026-05-31 11:11:28 -0600

    add service worker for PWA installability

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'koda-v1';
+const urlsToCache = ['/'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('/'))
+    );
+  }
+});
